3a41e1eb4a0dae17ef5064a72a55c7fd89f7a094
kwamentw <antwi1496@hotmail.com>
2024-07-23 21:49:17 +0100

    +vesting solution

---
+++ src/Vesting.sol
@@ -0,0 +1,97 @@
+// This code snippet is provided by Pessimistic company.
+// To apply for the internship opportunity at Pessimistic company,
+// please fill out the form by visiting the following link: https://forms.gle/SUTcGi8X86yNoFnG7
+
+// Caution: This code is intended for educational purposes only
+// and should not be used in production environments.
+
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.14;
+
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+
+contract Vesting {
+    // emits an event when the tokens are released after cliff period 
+    event TokenReleased(
+        address indexed account,
+        address indexed token,
+        uint256 amount
+    );
+
+    // the amount of locked funds and released funds
+    struct Info {
+        uint256 locked;
+        uint256 released;
+    }
+
+    address public immutable token; // token distributed
+    uint256 public immutable startTimestamp;// timestamp at which cliff starts
+    uint256 internal cliffDuration; // cliff duration 
+    uint256 internal vestingDuration;// vesting duration 
+
+    // holds information of the amount of released token and locked token
+    mapping(address => Info) internal _vesting;
+
+    uint256 public checkVar;
+
+    /**
+     * @notice constructor
+     * @param token_ - token address
+     * @param cliffMonthDuration - cliff duration in months
+     * @param vestingMonthDuration - vesting duration in months
+     * @param accounts - vesting accounts
+     * @param amounts - vesting amounts of accounts
+     **/
+    constructor(
+        address token_,
+        uint256 cliffMonthDuration,
+        uint256 vestingMonthDuration,
+        address[] memory accounts,
+        uint256[] memory amounts
+    ) {
+        // timestamp is initialised to the time protocol starts
+        startTimestamp = uint64(block.timestamp);
+        token = token_;
+        // cliff duration in weeks
+        cliffDuration = cliffMonthDuration * 4 weeks;
+        // vesting period in weeks
+        vestingDuration = vestingMonthDuration * 4 weeks;
+        // loading accounts
+        for (uint256 i = 0; i < accounts.length; i++) {
+            _vesting[accounts[i]] = Info({locked: amounts[i], released: 0});
+        }
+    }
+
+    function release() external {
+        //add history by block
+        address sender = msg.sender;
+        //check to make sure cliff period has ended 
+        require(
+            block.timestamp > startTimestamp + cliffDuration,
+            "cliff period has not ended yet."
+        );
+        // gets amount locked into vesting by user
+        Info storage vestingInfo = _vesting[sender];
+        // calculates amount to be taken every month till vesting is over
+        uint256 amountByMonth = vestingInfo.locked /
+            (vestingDuration + cliffDuration);
+
+        // amount of money to be released     
+        uint256 releaseAmount = ((block.timestamp - startTimestamp) / 4 weeks) *
+            amountByMonth -
+            vestingInfo.released;
+
+        require(releaseAmount > 0, "not enough release amount.");
+
+        // tracks the amount of money released/sent
+        vestingInfo.released += releaseAmount;
+        // sends money to user
+        SafeERC20.safeTransfer(IERC20(token), sender, releaseAmount);
+
+         checkVar = vestingInfo.released;
+    }
+}
+//@audit no emit
+//@audit small amounts will revert
+//@audit check whether cliff > vesting
+//@audit protocol can be drained because there is no check on= whether user has claimed all his tokens 
---
+++ test/vest.t.sol
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier:MIT
+pragma solidity ^0.8.14;
+
+import {Test} from "forge-std/Test.sol";
+import {console2}from "forge-std/console2.sol";
+import {Vesting} from "../src/Vesting.sol";
+import {tokenMock} from "../src/token.sol";
+
+contract vestTest is Test{
+    Vesting vest;
+    tokenMock token;
+
+    address deployer;
+    address bene=address(0x123);
+    address bene1=address(0x456);
+    uint256 timeDeployed;
+    function setUp() public {
+        deployer = address(this);
+        token = new tokenMock();
+
+        uint256 cliffMnthDuration = 2;
+        uint256 vestMnthDuration = 4;
+        
+
+        address[] memory accounts = new address[](2);
+        accounts[0]=bene;
+        accounts[1]=bene1;
+
+        uint256[] memory amounts = new uint256[](2);
+        amounts[0]=10000e4;
+        amounts[1] = 2000e18;
+
+        timeDeployed = block.timestamp;
+        vest = new Vesting(address(token),cliffMnthDuration,vestMnthDuration,accounts,amounts);
+
+        token.mint(address(vest),10000e18);
+    }
+
+    function testRelease() public{
+        vm.startPrank(bene1);
+        uint64 warptime =4 * 4 weeks;
+
+        vm.warp(warptime);
+        vest.release();
+        vm.stopPrank();
+    }
+
+    function test_checkRounding() public {
+        vm.startPrank(bene);
+        uint64 warptime = 1000000007 * 4 weeks;
+
+        vm.warp(warptime+timeDeployed);
+        vest.release();
+        console2.log(vest.checkVar());
+        vm.stopPrank();
+    }
+}
