30a60b8895b891dca117b54a248551289b08996d
kwamentw <antwi1496@hotmail.com>
2024-07-29 18:44:08 +0100

    +solution 3

--- src/Libraries.sol
+++ src/Libraries.sol
@@ -95,3 +95,38 @@ contract PlacementInitializer is ManagerStorage{
         record = _placementRegistry.placements[placementId];
     }
 }
+
+// solution #2 - Are the items at line 73 in the Placements contract added correctly? Why? (Give a detailed explanation)
+/**
+ * No they are not being added correctly and the root cause is from the placementId
+ * The issue is;
+ * the placementId variable is uninitialized 
+ * but from line 64 we can see that there is a placementIdtracker
+ * which is meant to track the value of placementid
+ * but here is the case placementId is not initialised to the tracker 
+ * making the values of the return value out of sync and incorrect
+ * This will result in unexpected behavior and errors when pushing items on line 73
+ * and also it will return an invalid value at the end of the function
+ * 
+ * //// BOUNS ////
+ * on line 74 the placement fee being added 
+ * the fee charged on all items will be the same for each item in the loop
+ * i see it is intended design 
+ * but shouldn't this design allow different charge for each item? since they are of different value
+ */
+
+// solution #3 - What is the purpose of using Items for ItemtId in Items library?
+/**
+ * From the library `Items`, on line 15
+ * we can observe that on the next line theres is a `using Items for ItemId;` statement there
+ * This means that this library makes use of a ItemId struct implemented by the same library i.e:
+ * the library declares `struct ItemId` with two fields of type bytes4 and bytes
+ * Moving on to the next struct declaration on line 23 in the library `Items`
+ * we can observe that the `struct Item` has two fields of type ItemId and uint256
+ * uint256 that is the standard data type we all know but
+ * ItemId is the same ItemId we declared on line 28
+ * How is this possible?
+ * Libraries create reusable code as a result the struct ItemId can be reused even by the same libary
+ * and it is valid to use in the same library when you declare the intent just like on line 16
+ * So the Purpose of line 16 is to facilitate the use of `ItemId` in `Item`
+ */
